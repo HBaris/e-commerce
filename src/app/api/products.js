f90fbd371512dfdc0f477220d6a0629cae0a1d20
HBado <nes77850@gmail.com>
2024-03-12 02:18:36 +0300

    .env file added for required keys mongodb package installed mongoose.js file added which is connection via database page.tsx values changed and optimized for now products.js file created inside of api directory

---
+++ src/app/api/products.js
@@ -0,0 +1,7 @@
+import { initMongoose } from "../../../lib/mongoose";
+
+
+export default async function handle(req, res) {
+
+    await initMongoose();
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
 
 export default function Home() {
   return (
-    <div>test</div>
+    <div className="p-5">
+      <div>
+        <h2 className="text-2xl font-bold">
+          Mobile Phones and Cool Gadgets
+        </h2>
+        <div className="py-4">
+          <div className="w-64">
+            <div className="bg-blue-100 p-5 rounded-xl">
+              <img src="/products/iphone.png" alt=""></img>
+            </div>
+
+            <div className="mt-2">
+              <h3 className="font-bold text-lg">Iphone 14 Pro</h3>
+            </div>
+
+            <p className="text-sm mt-2 leading-4"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia similique in enim autem eveniet reprehenderit, fugit, aut illum perspiciatis distinctio cumque consequatur assumenda ipsum iure veritatis, asperiores modi praesentium perferendis.</p>
+            <div className=" flex my-2 mx-1">
+              <div className="text-2xl font-bold mr-3 grow">$999</div>
+              <button className="bg-emerald-500 text white py-1 px-3 rounded-xl">+</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 }
